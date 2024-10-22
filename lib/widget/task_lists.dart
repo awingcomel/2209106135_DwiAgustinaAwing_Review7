723dd0b60585cd7b0968f7103a10274d6c3913ca
Dwi Agustina Awing <dwiagustinaawng@gmail.com>
2024-10-22 10:09:55 +0800

    review7

---
+++ lib/widget/task_lists.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:pertemuan7/controller/main_controller.dart';
+import 'package:pertemuan7/widget/task_tile.dart';
+import 'package:provider/provider.dart';
+
+class TaskList extends StatelessWidget {
+  const TaskList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<TaskData>(builder: (context, TaskData, child) {
+    return ListView.builder(
+        itemBuilder: (context, index) {
+          final task = TaskData.tasks[index];
+          return TaskTile(
+            taskTitle: task.name,
+            isChecked: task.isDone,
+            checkboxCallback: (checkboxState) {
+              TaskData.updateTask(task);
+            },
+            longPressCallback: () {
+              TaskData.deleteTask(task);
+            },
+          );
+        },
+        itemCount: TaskData.taskCount,
+      );
+    });
+  }
+}
